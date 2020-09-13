4b0b9ab4eda8d159707b28f179ceee7d9a66d552
Donald LAY <dlay@admantic.fr>
2020-09-13 19:32:05 +0200

    Depart: implement Depart.traiter()

--- src/Depart.cpp
+++ src/Depart.cpp
@@ -1,4 +1,5 @@
 #include "../include/Depart.h"
+#include "../include/Simulation.h"
 
 Depart::Depart(double heure_evenement, Caissier* caissier, Client* client):Evenement(heure_evenement, simulation) {
     this->heure_evenement = heure_evenement;
@@ -6,7 +7,13 @@ Depart::Depart(double heure_evenement, Caissier* caissier, Client* client):Evene
     this->client = client;
 }
 
-// TODO
 void Depart::traiter() {
-
+    FileAttente *file_clients = simulation->mBanque()->mFileAttente();
+    if (!file_clients->estVide()) {
+        Client *premier_client = file_clients->retirer();
+        caissier->servir(premier_client);
+    } else {
+        caissier->estDisponible();
+    }
+    client->~Client();
 }
