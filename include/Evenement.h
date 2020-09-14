a94f32cda6574631d86d2da0afe85d621cecdf85
Donald LAY <dlay@admantic.fr>
2020-09-14 02:14:12 +0200

    Evenement: add static link

--- include/Evenement.h
+++ include/Evenement.h
@@ -11,7 +11,7 @@ class Evenement {
         Evenement(double heure_evenement, Simulation* simulation);
 
         // Getter
-        double heureEvenement();
+        virtual double heureEvenement();
         
         virtual void traiter();
 };
